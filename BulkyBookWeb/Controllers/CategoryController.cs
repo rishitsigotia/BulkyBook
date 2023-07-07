27e08c9714f3d70cf78e5776c79d18c1dd608cd8
Rishit <rsigotia123@gmail.com>
2023-07-07 13:09:36 +0530

    Edit Category GET method implemented

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,8 +19,49 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
 
-        //GET
-        public IActionResult Create()
+		//GET
+		public IActionResult Edit(int? id)
+		{
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var CategoryFromdb = _db.Categories.Find(id); // as we have primary key this will be most suited
+            //var CategoryFromFirst = _db.Categories.FirstOrDefault(c => c.Id == id);  These are also method to retrive id // it will return first element of the list
+            //var CategoryFromSingle = _db.Categories.SingleOrDefault()  // it will not throw exception it will be just empty if element not found , it will throw if there are more than one element
+            
+            if(CategoryFromdb == null)
+            {
+                return NotFound();
+            }
+
+			return View(CategoryFromdb); //if id is found we will send it to view
+		}
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.TryAddModelError("Name", "The Name and display order cannot be same");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(obj);
+		}
+
+
+
+
+
+		//GET
+		public IActionResult Create()
         {
 
             return View();
