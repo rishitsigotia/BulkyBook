2825abc9ac873f87831a1e026e349fe0b1886148
Rishit <rsigotia123@gmail.com>
2023-07-20 15:56:53 +0530

    N-Tier Architecture Implemented

--- BulkyBook.Models/Category.cs
+++ BulkyBook.Models/Category.cs
@@ -1,21 +1,21 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace BulkyBookWeb.Models
+namespace BulkyBook.Models
 {
     public class Category
     {
         [Key]
-        public int  Id { get; set; }
-        
+        public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
 
         [DisplayName("Display Order")]  // it will now show like this "Display Order" previously it was DisplayOrder
-        [Range(1,100,ErrorMessage = "Enter the quantity b/w 1 to 100 !!")] // Range Validation Attribute
+        [Range(1, 100, ErrorMessage = "Enter the quantity b/w 1 to 100 !!")] // Range Validation Attribute
         public int DisplayOrder { get; set; }
 
-        
+
         public DateTime OrderCreated { get; set; } = DateTime.Now;
     }
 }
